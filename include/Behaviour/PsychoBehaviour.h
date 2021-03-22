cfc6e8b9d19ed6f9611dea448abe8771e42d66fa
Maher-OUALI <45775089+Maher-OUALI@users.noreply.github.com>
2021-03-22 20:09:51 +0100

    Update PsychoBehaviour.h

--- include/Behaviour/PsychoBehaviour.h
+++ include/Behaviour/PsychoBehaviour.h
@@ -4,12 +4,22 @@
 #include "Behaviour.h"
 #include "DumbBehaviour.h"
 #include "SpinnerBehaviour.h"
+#include "KamikazeBehaviour.h"
+#include "PrevoyanteBehaviour.h"
+#include "PeureuseBehaviour.h"
+#include "GregaireBehaviour.h"
+#include "BraindeadBehaviour.h"
 
 
 enum  PsycBehvrs
 {
     psychDumb,
-    psychSpinner
+    psychSpinner, 
+    psychBraindead,
+    psychKamikaze,
+    psychGregaire,
+    psychPeureuse,
+    psychPrevoyante
 };
 
 
@@ -18,6 +28,11 @@ class PsychoBehaviour:public Behaviour
      private:
      DumbBehaviour dumbBehaviour_;
      SpinnerBehaviour spinnerBehaviour_;
+     KamikazeBehaviour kamikazeBehaviour_;
+     PrevoyanteBehaviour prevoyanteBehaviour_;
+     PeureuseBehaviour peureuseBehaviour_;
+     GregaireBehaviour gregaireBehaviour_;
+     BraindeadBehaviour braindeadBehaviour_;
      Behaviour* currentBehaviour_;
     
     public:
