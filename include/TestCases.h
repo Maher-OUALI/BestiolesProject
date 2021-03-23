e1ff6bdfde4c2f8e23faec786d3da844a1f9bf8a
Maher-OUALI <45775089+Maher-OUALI@users.noreply.github.com>
2021-03-23 23:02:26 +0100

    Create TestCases.h

---
+++ include/TestCases.h
@@ -0,0 +1,30 @@
+#ifndef _TESTCASES_H_
+#define _TESTCASES_H_
+
+
+#include "Aquarium.h"
+#include "BestioleFactory.h"
+#include "Bestiole.h"
+#include "Config.h"
+
+namespace TestCases
+{
+
+void kamikazeBehaviourUnitTest(Aquarium & ecosystem);
+void gregaireBehaviourUnitTest(Aquarium & ecosystem);
+void prevoyanteBehaviourUnitTest(Aquarium & ecosystem);
+void peureuseBehaviourUnitTest(Aquarium & ecosystem);
+void psychoBehaviourUnitTest(Aquarium & ecosystem);
+
+void earsSensorUnitTest(Aquarium & ecosystem);
+void eyesSensorUnitTest(Aquarium & ecosystem);
+void compositeSensorUnitTest(Aquarium & ecosystem);
+
+void cloakAccessoryUnitTest(Aquarium & ecosystem);
+void shieldAccessoryUnitTest(Aquarium & ecosystem);
+void turboJetAccessoryUnitTest(Aquarium & ecosystem);
+
+
+}
+
+#endif
