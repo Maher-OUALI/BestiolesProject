3c6a2d3528ab29f7f1d76d9dfe72cefcf860b881
Hamdi Mohamed <50010858+mohamed-hamdi@users.noreply.github.com>
2021-03-24 10:33:13 +0100

    Update Ears.h

--- include/Sensor/Ears.h
+++ include/Sensor/Ears.h
@@ -17,6 +17,13 @@ public:
         detection_capacity=MyRandomGen::RandomDouble(EnvConfig::sMinHearingIntensity , EnvConfig::sMaxHearingIntensity);
         max_dist=MyRandomGen::RandomDouble(EnvConfig::sMinHearingDist , EnvConfig::sMaxHearingDist);
     }
+    
+    Ears(const Ears &ear):Sensor("Ears")
+    {
+        max_dist=ear.max_dist;
+        detection_capacity=ear.detection_capacity;
+    }
+
     bool canSense(const Bestiole &b2) override;
 
     Ears* clone(Bestiole* ownr) ;
