0cad0a34d5e2a8446b788fd1236b1587f55033a2
ilan <ilumatiasklei@hotmail.com>
2021-03-24 08:09:23 +0100

    comments

--- src/randomGen.cpp
+++ src/randomGen.cpp
@@ -2,18 +2,6 @@
 
 
 
-// template<typename T>
-// T MyRandomGen::getRandomType(std::vector<T> types,std::vector<double> probs)
-//     {
-//     std::random_device rd;
-//         std::mt19937 gen(rd());
-//         std::discrete_distribution<> d(probs.begin(),probs.end());
-//         auto res=types[d(gen)];
-//         std::cout << res << std::endl;
-//         return res;
-//     }
-
-
 //Random boolean generator
 bool MyRandomGen::IsTrueRandom(double prob_true)
 {
@@ -31,5 +19,3 @@ double MyRandomGen::RandomDouble(double b_min , double b_max)
 
    return dis(gen);
 }
-//template MyEnum MyRandomGen::getRandomType<MyEnum>(std::vector<MyEnum>,std::vector<double>);
-//template PsycBehvrs MyRandomGen::getRandomType<PsycBehvrs>(std::vector<PsycBehvrs>,std::vector<double>);
